166d6b4baad8023e1a1b1bb894a45287733536b3
Justo Manrique Urbina <ja.manrique@pm.me>
2019-07-23 23:13:20 -0500

    ab

---
+++ kaggle/boston_crimes.jl
@@ -0,0 +1,14 @@
+cd("C://Users/jaman//Documents//GitHub//jm-learning//kaggle/crimes-in-boston")
+using CSV, DataFrames
+boston_cr = CSV.read("crime.csv",copycols=true)
+crimes_master = CSV.read("offense_codes.csv",copycols=true)
+
+# Limpiar el objeto crimes_master
+unique!(crimes_master, :CODE)
+sort!(crimes_master, :CODE)
+
+boston_cr = join(boston_cr,crimes_master, on = :OFFENSE_CODE => :CODE, kind = :left)
+
+# Creación de función summary, para conocer tipos de la variable
+summary(df) = hcat(names(df),eltypes(df),vec(convert(Matrix,first(boston_cr,1))))
+summary(boston_cr)
